399ab010990bf8903ebc8a18df335f9dc0cab2cf
RIKOje <40830087+RIKOje@users.noreply.github.com>
2021-04-16 11:30:04 +0200

    ich habs bearbeitet

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class main {
     System.out.println("Hello World");
     System.out.println("Hallo nochmal");
     System.out.println("Hallo zum 3. mal");
+    System.out.println("Servus");
   }
 }
