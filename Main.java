00eba164495abcf4dd5c8affd73e09e00710207e
lukas256-tech <schildrakete@web.de>
2021-04-16 14:20:45 +0200

    Wer kann das lesen?

--- Main.java
+++ Main.java
@@ -2,5 +2,17 @@ public class Main {
 
   public static void main (String[] args){
     System.out.println("Hello World");
+    System.out.println("Wer Kann das lesen?");
+    String auswahl = Kon.readString();
+    switch (auswahl) {
+      case "Joshua":
+      case "joshua":
+        break;
+      case "Emil":
+      case "emil":
+        break;
+      default:
+      break;
+    }
   }
 }
