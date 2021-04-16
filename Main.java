df46dc15b6e241e11e008336e7539bf39e05aa7f
lukas256-tech <schildrakete@web.de>
2021-04-16 14:24:57 +0200

    überarbeitet

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
   public static void main (String[] args){
     System.out.println("Hello World");
-    System.out.println("Wer Kann das lesen?");
+    System.out.println("Wer kann das lesen?");
     String auswahl = Kon.readString();
     switch (auswahl) {
       case "Joshua":
---
+++ WriteToFile.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class WriteToFile {
+  public static void main(String[] args) {
+    try {
+      FileWriter myWriter = new FileWriter("Libary.txt");
+      boolean abfrage = true;
+      int i = 1;
+        while(abfrage){
+          i++;
+          myWriter.write(i + "\n");
+          if(i/100000000 == 100000000){
+            abfrage = false;
+          }
+        }
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
