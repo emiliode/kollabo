56938bab9ecd0c21bc7081437a8055c1f62b5c8a
RIKOje <40830087+RIKOje@users.noreply.github.com>
2021-04-16 11:24:34 +0200

    Hallo

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
   public static void main (String[] arg){
     System.out.println("Hello World");
     System.out.println("Hallo nochmal");
+    System.out.println("Hallo zum 3. mal");
   }
 }
