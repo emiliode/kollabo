92d590bc96b5ebdbd39b892f029ae42bdc505c4e
YupsiFuchsi <jocofelten@gmail.com>
2021-04-16 11:54:50 +0200

    neu

--- main.java
+++ main.java
@@ -1,12 +1,6 @@
 public class Main {
 
-  public static void main (String[] arg){
+  public static void main (String[] args){
     System.out.println("Hello World");
-    System.out.println("Hallo nochmal");
-    System.out.println("Hallo zum 3. mal");
-    System.out.println("Servus");
-    // ein toller neuer Kommentar
-    //zwei tolle Kommentare
-    //bin ich ich?
   }
 }
