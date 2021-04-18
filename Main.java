bd30285292710133d7a1faea9f5624463a80600b
YupsiFuchsi <jocofelten@gmail.com>
2021-04-18 13:45:32 +0200

    Kon Datei entfernt und mit Scanner ersetzt

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 public class Main {
 
   public static void main (String[] args){
-    // switch 
+    Scanner mySc = new Scanner(System.in);
     System.out.println("Hello World");
     System.out.println("Wer kann das lesen?");
-    String auswahl = Kon.readString();
+    String auswahl = mySc.nextLine();
     switch (auswahl) {
       case "Joshua":
       case "joshua":
